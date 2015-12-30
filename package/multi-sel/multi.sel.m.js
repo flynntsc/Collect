ce4e1d9f87e23d3647d85f3da1b8e0400bc75026
flynntsc <flynntsc@qq.com>
2015-12-30 15:59:15 +0800

    edit

--- package/multi-sel/multi.sel.m.js
+++ package/multi-sel/multi.sel.m.js
@@ -1,65 +1,63 @@
 'use ctrict'
 function MultiSel(el) {
-	this.sel = document.querySelector(el);
-	this.int();
+    this.sel = document.querySelector(el);
+    this.arg = this.int();
+    this.watch();
 }
 MultiSel.prototype.int = function() {
-	var sel = this.sel,
-		box = document.createElement('div'),
-		ipt = document.createElement('div'),
-		opt = sel.options || [],
-		opl = opt.length,
-		txt = '请选择';
-	box.style.position = 'relative';
-	box.style.display = 'inline-block';
-	box.style.boxSizing = 'border-box';
-	box.style.width = '100%';
-	ipt.setAttribute('contenteditable','true');
-	sel.parentNode.insertBefore(box,sel);
-	box.appendChild(sel);
-	box.appendChild(ipt);
-	sel.style.position = 'absolute';
-	sel.style.width = '100%';
-	sel.style.height = '100%';
-	sel.style.opacity = '0';
-	sel.style.zIndex = '1';
-	saveVal(opt,opl,ipt);
-	sel.addEventListener('change',function() {
-		var opt = this.options,
-			opl = opt.length,
-			ipt = this.nextSibling;
-		saveVal(opt,opl,ipt);
-	},false);
+    var sel = this.sel,
+        box = document.createElement('div'),
+        ipt = document.createElement('div'),
+        opt = sel.options || [],
+        opl = opt.length,
+        txt = '请选择';
+    box.style.position = 'relative';
+    box.style.display = 'inline-block';
+    box.style.boxSizing = 'border-box';
+    box.style.width = '100%';
+    ipt.setAttribute('contenteditable','true');
+    sel.parentNode.insertBefore(box,sel);
+    box.appendChild(sel);
+    box.appendChild(ipt);
+    sel.style.position = 'absolute';
+    sel.style.width = '100%';
+    sel.style.height = '100%';
+    sel.style.opacity = '0';
+    sel.style.zIndex = '1';
+    return [opt,opl,ipt];
 }
-MultiSel.prototype.refresh = function() {
-	var opt = this.sel.options,
-		opl = opt.length,
-		ipt = this.sel.nextSibling;
-	saveVal(opt,opl,ipt);
+MultiSel.prototype.watch = function() {
+	var that = this;
+	saveVal(that.arg);
+    this.sel.addEventListener('change',function() {
+        saveVal(that.arg);
+    },false);
 }
 MultiSel.prototype.addClass = function(cla) {
-	this.sel.nextSibling.setAttribute('class',cla);
+    this.sel.nextSibling.setAttribute('class',cla);
 }
 MultiSel.prototype.css = function(obj) {
-	toStyle(this.sel.nextSibling,obj);
+    toStyle(this.sel.nextSibling,obj);
 }
 // 选中值赋值
-function saveVal(opt,opl,ipt) {
-	var str = '';
-	while(opl>0) {
-		opl--;
-		if(opt[opl].selected) {
-			console.info(opt[opl].innerHTML);
-			str += opt[opl].innerHTML+',';
-		}
-	}
-	str.length > 0 ? str = str.substring(0,str.length-1) : str = '请选择(可多选)';
-	ipt.innerHTML = str;
+function saveVal(args) {
+    var opt = args[0],
+    opl = args[1],
+    ipt = args[2],
+    arr = [],
+    str = '';
+    for(var i = 0; i < opl; i++) {
+    	if(opt[i].selected) {
+        	arr.push(opt[i].innerHTML);
+    	}
+    }
+    str = arr.length > 0 ? arr.join('、') : '请选择(可多选)';
+    ipt.innerHTML = str;
 }
 function toStyle(el,cssobj) {
-	for(var x in cssobj) {
-		if(cssobj.hasOwnProperty(x)) {
-			el.style[x] = cssobj[x];
-		}
-	}
+    for(var x in cssobj) {
+        if(cssobj.hasOwnProperty(x)) {
+            el.style[x] = cssobj[x];
+        }
+    }
 }
